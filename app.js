b2daf4db39733109db431ac54d8af14981a505d3
Ulises Lara <ulara@nearsoft.com>
2019-09-27 17:15:12 -0500

    Removed test code

--- app.js
+++ app.js
@@ -4,15 +4,6 @@ const Channel = require('./models/channel')
 var mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
 
-const karmabotData = {
-  enabled:true,
-  channelId:null,
-  users:{
-    'U152VABBJ':5,
-    'UNTRAJLLU':2
-  }
-};
-
 // Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
