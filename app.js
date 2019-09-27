b17147b9df8433b0190960e6a353e6bc0106ec59
Sandra Herrera <sandra.h.arriaga@gmail.com>
2019-09-27 17:52:00 -0500

    top and bottom functions

--- app.js
+++ app.js
@@ -10,14 +10,17 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+const botId = 'UNWGTHC5C';
 app.message('hello', ({ message, say }) => {
   // say() sends a message to the channel where the event was triggered
   say(`Hey there <@${message.user}>!`);
+
   Channel.find({
-    channelId: 'TEST'
+    channelId: 'CNUBMHXUY'
   }).then(channel => {
     console.log(channel)
   });
+  
 });
 
 app.message('help', ({ message, say }) => {
@@ -27,17 +30,80 @@ app.message('help', ({ message, say }) => {
 });
 
 app.message('enable', ({ message, say }) => {
-  console.log(message);
-  const channel = new Channel({
-    channelId: message.channel,
-    enabled: true
-  });
-  channel.save().then(() => say(`Bot enabled`));
-  
+  say(`Bot enabled`);
 });
 
 app.message('disable', ({ message, say }) => {
   say(`Bot disabled, if you want to enable, type 'enable'`);
+});
+
+app.message('top', ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  if(message.text.indexOf(botId) == 2)
+  {
+    //say(`Hey there <@${message.user}>! there is a list with the top users`);
+    Channel.findOne({
+      channelId: message.channel
+    }).then(channel => {
+    
+      var arrayUsers = channel.users
+
+      var keys = [];
+    for(var k in channel.users) keys.push(k);
+      let len = keys.length;
+      for (let i = 0; i < len; i++) {
+        
+          for (let j = 0; j < len; j++) {
+              if (arrayUsers[keys[j]] < arrayUsers[ keys[j+ 1] ]) {
+                  let tmp = arrayUsers[keys[j]];
+                  arrayUsers[keys[j]] = arrayUsers[keys[j+ 1] ];
+                  arrayUsers[keys[j+ 1] ] = tmp;
+              }
+          }
+      }
+      var messageToUser = ""
+      for (let i = 0; i < len; i++) {
+        messageToUser += `<@${keys[i]}>: ${arrayUsers[keys[i]]}\n`
+      }
+      say(`Hey there <@${message.user}>! there is a list with the top users\n` + messageToUser);
+    });
+  }
+ 
+});
+
+app.message('bottom', ({ message, say }) => {
+  if(message.text.indexOf(botId) == 2)
+  {
+ // say() sends a message to the channel where the event was triggered
+ Channel.findOne({
+  channelId: message.channel
+}).then(channel => {
+
+  var arrayUsers = channel.users
+
+  var keys = [];
+for(var k in channel.users) keys.push(k);
+  let len = keys.length;
+  for (let i = 0; i < len; i++) {
+    
+      for (let j = 0; j < len; j++) {
+          if (arrayUsers[keys[j]] > arrayUsers[ keys[j+ 1] ]) {
+              let tmp = arrayUsers[keys[j]];
+              console.log(tmp)
+              arrayUsers[keys[j]] = arrayUsers[keys[j+ 1] ];
+              arrayUsers[keys[j+ 1] ] = tmp;
+          }
+
+      }
+  }
+  var messageToUser = ""
+  for (let i = 0; i < len; i++) {
+    messageToUser += `<@${keys[i]}>: ${arrayUsers[keys[i]]}\n`
+  }
+  say(`Hey there <@${message.user}>! there is a list with the bottom users\n` + messageToUser);
+});
+  }
+
 });
 
 (async () => {
