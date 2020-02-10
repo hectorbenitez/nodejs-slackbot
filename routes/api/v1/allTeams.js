7c1c8bf08636567366d04821afb278d5908cdee9
Luis Mendieta <lmendieta@nearsoft.com>
2020-02-10 15:58:32 -0600

    Add a new route to get all teams

---
+++ routes/api/v1/allTeams.js
@@ -0,0 +1,13 @@
+const Team = require('../../../models/team')
+
+module.exports = app => {
+  app.receiver.app.get('/api/v1/teams/all', async (req, res) => {
+    try {
+      const teams = await Team.find()
+      res.json(teams)
+    } catch (error) {
+      console.error('command score:', error)
+      res.status(500).send('Internal Server Error')
+    }
+  })
+}
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@
 module.exports = (app) => {
   require('./installApp')(app)
   require('./api/v1/userTeams')(app)
+  require('./api/v1/allTeams')(app)
   require('./api/v1/teamActivity')(app)
   require('./api/v1/teamScore')(app)
 
