c593b61c57835d94c16770b47996caa0c5fd49dc
Luis Mendieta <lmendieta@nearsoft.com>
2020-02-10 15:33:57 -0600

    Add a default response for root route

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,8 @@ module.exports = (app) => {
   require('./api/v1/userTeams')(app)
   require('./api/v1/teamActivity')(app)
   require('./api/v1/teamScore')(app)
+
+  app.receiver.app.get('/', (req, res) => {
+    res.send('ok')
+  })
 }
