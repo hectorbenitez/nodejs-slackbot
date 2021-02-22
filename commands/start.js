129881e3d8c2f5a2825c5cc0cd3f233cbd9c20fd
Raul Yau <ryau07@gmail.com>
2021-02-22 11:33:50 -0600

    display questions with possible answers

--- commands/start.js
+++ commands/start.js
@@ -19,6 +19,7 @@ module.exports = app => {
     const command = message.text.match(/start [\w]+$/)[0]
     const splitedCommand = command.split(" ")
     const startAction = splitedCommand[1]
+    
     switch(startAction){
 
       case startOptions.SURVEY:
@@ -28,14 +29,19 @@ module.exports = app => {
           _idUser: "602e9a7b8aea9d0815546fd9",
           _idSurvey: "602ae89e21697a369af0fdc6"
         }
-
+        
         fetch(url, {
           method: 'POST',
           body: JSON.stringify(data),
           headers: { 'Content-Type': 'application/json' }
-        }).then(res => res.json())
-          .then(json => console.log(json));
-
+        }).then(async res => await res.json())
+          .then(json => { console.log(json.survey)
+                          //say(`You are now starting ${json.survey.surveyName} survey.`)
+                          json.survey.questions.forEach(surveyQuestion => {
+                            say(`Q : ${surveyQuestion.question} A: | ${ surveyQuestion.answers } |`)
+                          });
+                        });
+        
         //const survey = splitedCommand[2]
         break
 
