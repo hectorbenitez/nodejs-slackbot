47abba6abf56f6cb2a1843cdb7ad0256504cf5df
Hector Benitez <hector.benitez.fletes@gmail.com>
2021-03-03 08:33:46 -0600

    error in middleware

--- middlewares/noDirectMention.js
+++ middlewares/noDirectMention.js
@@ -1,8 +1,8 @@
 module.exports = async ({ message, context, next }) => {
   if (context.botUserId === undefined) {
     throw new ContextMissingPropertyError(
-      'botUserId',
-      'Cannot match direct mentions of the app without a bot user ID. Ensure authorize callback returns a botUserId.',
+      "botUserId",
+      "Cannot match direct mentions of the app without a bot user ID. Ensure authorize callback returns a botUserId."
     );
   }
 
@@ -10,10 +10,13 @@ module.exports = async ({ message, context, next }) => {
     await next();
   }
 
-  const botMention = `<@${context.botUserId}>`
-  const text = message.text.trim();
-  if (text.startsWith(botMention)) {
-    return;
+  const botMention = `<@${context.botUserId}>`;
+
+  if (message.text) {
+    const text = message.text.trim();
+    if (text.startsWith(botMention)) {
+      return;
+    }
   }
 
   await next();
