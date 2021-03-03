2de5dcd15b1b59f1474a4e20da5192247e5d71a1
Hector Benitez <hector.benitez.fletes@gmail.com>
2021-03-03 00:37:32 -0600

    remove typo

--- actions/surveyAnswer.js
+++ actions/surveyAnswer.js
@@ -4,7 +4,7 @@ const TriviaGame = require('../models/triviaGame')
 const { directMention } = require('@slack/bolt')
 
 module.exports = app => {
-  app.action('survey-answer', async ({ ack, body, action, ack, context, say }) => {
+  app.action('survey-answer', async ({ ack, body, action, context, say }) => {
     console.log(body, action, context)
     ack();
     say('Test')
